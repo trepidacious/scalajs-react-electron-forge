7a287f2068fd595693823b6dd231986465797380
trepidacious <trepidacious@gmail.com>
2018-08-12 12:20:56 +0100

    Scalajs-react component in page
    Needs an ugly import - looking for a way to do this from scalajs instead.

--- scala_project_root/js/src/main/scala/org/rebeam/Main.scala
+++ scala_project_root/js/src/main/scala/org/rebeam/Main.scala
@@ -1,10 +1,18 @@
 package org.rebeam
 
+import japgolly.scalajs.react._
+import japgolly.scalajs.react.vdom.html_<^._
+import org.scalajs.dom
+
 object Main {
+
+  val MainView =
+    ScalaComponent.builder[String]("MainView")
+      .render_P(name => <.div("Hello ", name))
+      .build
+
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
-    println("Hello world!")
-    println("Hello world!")
-    println("Hello world!")
+    // Render the top-level view to the predefined HTML div with id "App"
+    MainView("World").renderIntoDOM(dom.document.getElementById("App"))
   }
 }
