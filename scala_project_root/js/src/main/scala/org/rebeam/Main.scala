0bf8a1604df4cadbf9b7069d0459b8e0355e8331
trepidacious <trepidacious@gmail.com>
2018-09-02 22:39:30 +0100

    Theming - use a dark theme to demonstrate

--- scala_project_root/js/src/main/scala/org/rebeam/Main.scala
+++ scala_project_root/js/src/main/scala/org/rebeam/Main.scala
@@ -1,5 +1,7 @@
 package org.rebeam
 
+import scalajs.js
+
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
 import org.scalajs.dom
@@ -16,16 +18,29 @@ object Main {
 
   @JSExport
   def main(): Unit = {
+
+    val theme = mui.styles.Styles.createMuiTheme(
+      js.Dynamic.literal(
+        "palette" -> js.Dynamic.literal(
+          "type" -> "dark"
+        )
+      )
+    )
+
     // Render the top-level view to the predefined HTML div with id "App"
-    <.div(
-      ^.margin := "20px",
-      MultiSelectDemo.component(MultiSelectDemo.Props(MultiSelectDemo.countries))
-    // DownshiftMultiDemo.ctor(DownshiftMultiDemo.Props(DownshiftDemo.countries))
-    // MainView("World").renderIntoDOM(dom.document.getElementById("App"))
-    // DownshiftDemo.ctor(DownshiftDemo.Props(DownshiftDemo.countries)).renderIntoDOM(dom.document.getElementById("App"))
-    // TextFieldDemo.ctor().renderIntoDOM(dom.document.getElementById("App"))
-      
+    // Note MuiThemeProvider should only have one child.
+    mui.MuiThemeProvider(theme = theme: js.Any)(
+      <.div(
+        ^.margin := "20px",
+        MultiSelectDemo.component(MultiSelectDemo.Props(MultiSelectDemo.countries)),
+      // DownshiftMultiDemo.ctor(DownshiftMultiDemo.Props(DownshiftDemo.countries))
+      // MainView("World")
+      // DownshiftDemo.ctor(DownshiftDemo.Props(DownshiftDemo.countries))
+        TextFieldDemo.ctor(),
+        mui.Card(component = "div":js.Any, raised = true, elevation = 1)(<.span("Hi!"))
+      )
     ).renderIntoDOM(dom.document.getElementById("App"))
+
     ()
   }
 }
